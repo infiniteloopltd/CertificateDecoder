71fa75b6267c588b0c8a571a6040bab14b29978a
fiach <fiach.reid@gmail.com>
2020-05-11 17:57:44 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Org.BouncyCastle.X509;
+
+namespace CertDecode1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var certificateBase64 = File.ReadAllText("cert.txt");
+            var certData = Convert.FromBase64String(certificateBase64);
+            var parser = new X509CertificateParser();
+            var cert = parser.ReadCertificate(certData);
+            Console.WriteLine(cert.SubjectDN);
+        }
+    }
+}
